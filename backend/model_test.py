dd3891dfd99d1ec9d74cf3def30eeea31b7fdad0
MohamedAmineBenAmmar <ma.benammar5@gmail.com>
2024-01-16 03:08:36 +0100

    fixed image dimensions error

--- backend/model_test.py
+++ backend/model_test.py
@@ -6,13 +6,20 @@ from factory import Factory
 def classify(imgname: str, model_name: str) -> (str, int):
     factory = Factory()
     
-    img_path = f"uploads/{imgname}" 
-    img = load_img(img_path, target_size=(48, 48))
-
+    img_path = f"uploads/{imgname}"     
     
+    # Load the image in grayscale mode (this will result in one channel)
+    img = load_img(img_path, color_mode="grayscale", target_size=(48, 48))
+    
+    # Convert the image to a numpy array
     img_array = img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0
+    
+    # Reshape the image to add a batch dimension, since Keras expects this shape (batch_size, height, width, channels)
+    img_array = img_array.reshape((1, 48, 48, 1))
+    
+    # Normalize the image
+    img_array = img_array.astype('float32')
+    img_array = img_array / 255.0
 
 
     loaded_model = factory.create(model_name)
